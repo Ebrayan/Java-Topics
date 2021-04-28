90232e4a926b227cb66dcb0bdf9dd3272e0f65ed
Ebrayan <brayanrd2013@gmail.com>
2021-04-28 05:07:54 -0400

    Merge origin/main into main
    Conflicts:
    	README.md

---
+++ OneDrive/Documentos/NetBeansProjects/ForCertification/src/forcertification/TestingColletions.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forcertification;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ *
+ * @author braya
+ */
+public class TestingColletions {
+    
+    
+    public static void main(String[] args) {
+        List<List> lista =  new ArrayList<>();
+        lista.add(lista);
+    }   
+}
